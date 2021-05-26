1778678547f74074ac85d342d197e75700d6d2f6
Reva Weil <revaw613@gmail.com>
2021-05-25 22:53:44 -0700

    Final

--- HW5.25.21WebScraper.Scraper/Scraper.cs
+++ HW5.25.21WebScraper.Scraper/Scraper.cs
@@ -31,40 +31,6 @@ namespace HW5._25._21WebScraper.Scraper
                 }
                 results.Add(scoopPost);
             }
-            //foreach (IElement result in searchResultElements)
-            //{
-            //    var title = result.QuerySelector("div.post.h2");
-            //    if (title == null)
-            //    {
-            //        continue;
-            //    }
-
-            //var scoopPost = new ScoopPost();
-            //scoopPost.Title = title.TextContent;
-
-            //var priceSpan = result.QuerySelector("span.a-offscreen");
-            //if (priceSpan != null)
-            //{
-            //    //scoopPost.Price = double.Parse(priceSpan.TextContent.Replace("$", String.Empty));
-            //}
-
-            //var imageElement = result.QuerySelector("img.s-image");
-            //if (imageElement != null)
-            //{
-            //    var imageSrcValue = imageElement.Attributes["src"].Value;
-            //    scoopPost.ImageUrl = imageSrcValue;
-            //}
-
-            //var anchorTag = result.QuerySelector("a.a-link-normal.a-text-normal");
-            //if (anchorTag != null)
-            //{
-            //    //scoopPost.LinkUrl = anchorTag.Attributes["href"].Value;
-            //}
-
-            //    results.Add(scoopPost);
-
-
-            //}
 
             return results;
         }
